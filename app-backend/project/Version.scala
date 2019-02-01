6bbc0273a9b739464a78ef3566d808d0a7087969
Grigory Pomadchin <gr.pomadchin@gmail.com>
2019-02-01 11:08:29 +0300

    Bump geotrellis-server version

--- app-backend/project/Version.scala
+++ app-backend/project/Version.scala
@@ -25,7 +25,7 @@ object Version {
   val findbugAnnotations = "3.0.1u2"
   val geotools = "17.1"
   val geotrellis = "2.2.0"
-  val geotrellisServer = "0.1.4"
+  val geotrellisServer = "0.1.5"
   val hadoop = "2.8.4"
   val hikariCP = "3.2.0"
   val http4s = "0.20.0-M4"
